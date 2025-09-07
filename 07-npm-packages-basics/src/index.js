18ae86b4a1eaaa94078c70b6af69ca0eadde32b6
Julio Siqueira <siqueiragomes75@gmail.com>
2025-09-07 16:23:05 -0300

    Atualizando conteudos de estudos

---
+++ 07-npm-packages-basics/src/index.js
@@ -0,0 +1,15 @@
+
+import chalk from 'chalk';
+import logSymbols from 'log-symbols';
+
+
+console.log(chalk.blue('Hello, World!'));
+
+console.log(logSymbols.success, chalk.green('Success!'));
+console.log(logSymbols.error, chalk.red('Error!'));
+console.log(logSymbols.warning, chalk.yellow('Warning!'));
+
+
+
+
+
